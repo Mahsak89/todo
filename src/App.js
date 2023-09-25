221551ccb8f8e219c12bff60dda8f5f601321304
Mahsak89 <>
2023-09-25 20:51:01 +0000

    fiexed error of not connecting to api

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import NavBar from './components/NavBar';
 import styles from "./App.module.css";
 import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
-import SignUpForm from './pages/auth/SignUpForm';
+import "./api/axiosDefaults";
+import SignUpForm from "./pages/auth/SignUpForm";
+
 
 function App() {
   return (
@@ -11,8 +13,8 @@ function App() {
      <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign in</h1>} />
-          <Route exact path="/signup" render={() => <SignUpForm/>} />
+          <Route exact path="/signin" render={() => <h1>login page</h1>} />
+          <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://productive-c40034b33280.herokuapp.com/";
+axios.defaults.baseURL ="https://productive-c40034b33280.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
