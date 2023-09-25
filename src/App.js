c7ef5e6df97528f71481e73afb65aefd61b9e3c1
Mahsak89 <>
2023-09-25 18:20:29 +0000

    added navbar component and added styles to app.module.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import NavBar from './components/NavBar';
+import './App.module.css';
 
 function App() {
   return (
     <div className="App">
-     <h1>hi</h1>
+     <NavBar/>
     
     </div>
   );
---
+++ src/components/NavBar.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import Navbar from "react-bootstrap/Navbar"
+import Container from "react-bootstrap/Container"
+import Nav from "react-bootstrap/Nav"
+import logoo from "../assets/logoo.png";
+
+
+
+const NavBar = () => {
+  return (
+    <Navbar expand="md" fixed="top">
+      <Container>
+        <Navbar.Brand>
+          <img src={logoo} alt="logo" height="35" />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="ml-auto text-left">
+            <Nav.Link>
+              <i className="fas fa-home"></i>Home
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-sign-in-alt"></i>Sign in
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fas fa-user-plus"></i>Sign up
+            </Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavBar
